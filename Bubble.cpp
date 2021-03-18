da62009a068f7640056300ce807299e27f625a59
Shivangi Chauhan <68328033+Selcouth134@users.noreply.github.com>
2021-03-18 10:29:55 +0530

    Add files via upload

---
+++ Bubble.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void swap(int*a, int*b)
+{
+int temp;
+temp= *a;
+*a = *b;
+*b = temp;
+}
+void display(int arr[], int n)
+{
+    for(int i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+}
+int main()
+{
+    int n,i,j;
+    cout<<"Enter Limit\n";
+    cin>>n;
+    int arr[n];
+    for(i=0;i<n;i++)
+    cin>>arr[i];
+
+for(i=0;i<n;i++)
+{
+    for(j=0;j<n-i-1;j++)
+    {
+        if (arr[j]>arr[j+1])
+        swap(&arr[j], &arr[j+1]);
+    
+    }
+}
+display(arr,n);
+}
---
+++ Selection.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+void swap(int*a, int*b)
+{
+int temp;
+temp= *a;
+*a = *b;
+*b = temp;
+}
+void display(int arr[], int n)
+{
+    for(int i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+}
+int main()
+{
+    int n,i,j,temp,index;
+    cin>>n;
+    int arr[n];
+    for(i=0;i<n;i++)
+    cin>>arr[i];
+
+for(i=0;i<n-1;i++)
+{
+    temp=arr[i];
+    index=i;
+    for(j=i+1;j<n;j++)
+    {
+        if(arr[j]<temp){
+          temp=arr[i];
+            index=j;
+
+    }
+}
+swap(&arr[i], &arr[index]);
+}
+display(arr,n);
+}
